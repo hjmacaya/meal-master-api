7aa61c67c611affecf0c83c49f3463fc3fc02bab
Hernan Macaya <hjmacaya@uc.cl>
2024-02-18 19:25:30 -0300

    add: user's in bodies index

--- app/controllers/users/in_bodies_controller.rb
+++ app/controllers/users/in_bodies_controller.rb
@@ -1,2 +1,8 @@
 class Users::InBodiesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @in_bodies = current_user.in_bodies
+    render json: @in_bodies, status: :ok
+  end
 end
