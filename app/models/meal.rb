96881f64f28a2d5b2886e3013737b23f31df6832
Hernan Macaya <hjmacaya@uc.cl>
2024-02-19 03:16:18 -0300

    add: validations to meals attributes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,5 @@
 class Meal < ApplicationRecord
+  # Validations
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :protein_portions, :cereals_portions, :vegetables_portions, :fruit_portions, :fat_portions, :dairy_portions, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
